9faf4dfea6893396f2f30a242f0ad6b400cd4107
小朋 <2810580330@qq.com>
2020-06-15 15:19:38 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 rcgo --conf-path=/etc/ct/.aria2/aria2.conf -D
-nginx
-/etc/ct/sunny clientid 5ca48645cdcdeda5
+
+nohup /etc/ct/sunny clientid 5ca48645cdcdeda5 >/etc/ct/id.txt 2>&1 &
+
+/etc/ct/cloudreve
 tail -f /dev/null
