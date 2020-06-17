efe0581f497cafc5f2831b2c09a53bec52222a18
小朋 <2810580330@qq.com>
2020-06-18 00:31:38 +0800

    Update delete.aria2.sh

--- delete.aria2.sh
+++ delete.aria2.sh
@@ -9,7 +9,7 @@
 # Blog: https://p3terx.com
 #=================================================================
 
-DOWNLOAD_PATH='/root/Download'
+DOWNLOAD_PATH='/dev/down'
 
 FILE_PATH=$3
 RELATIVE_PATH=${FILE_PATH#${DOWNLOAD_PATH}/}
