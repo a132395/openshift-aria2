c097d3fdeac09d33113502ab87832f437f56dba7
小朋 <2810580330@qq.com>
2020-07-03 14:32:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #显示时间
 date
-bash <(curl -fsSL git.io/tracker.sh) "/etc/ct/.aria2/aria2.conf"
+#bash <(curl -fsSL git.io/tracker.sh) "/etc/ct/.aria2/aria2.conf"
 rcgo --conf-path=/etc/ct/.aria2/aria2.conf
 #tail -f /dev/null
