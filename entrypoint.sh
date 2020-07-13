c0290768f585b224ef32aebd8cc92c5b55ba9ec5
小朋 <2810580330@qq.com>
2020-07-13 17:38:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 #显示时间
 date
 bash <(curl -fsSL https://raw.githubusercontent.com/P3TERX/aria2.conf/master/tracker.sh) "/root/.aria2/aria2.conf"
-sed -i "s/#export RCLONE_CONFIG/export RCLONE_CONFIG/g" /root/.aria2/rclone.env
+sed -i "s/drive-name=OneDrive/drive-name=gd/g" /root/.aria2/script.conf
 aria2c --conf-path=/root/.aria2/aria2.conf
