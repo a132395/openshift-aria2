3e52a1ff8ee53361b2286954046ff2787d657935
小朋 <2810580330@qq.com>
2020-07-13 17:10:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 #显示时间
 date
 bash <(curl -fsSL https://raw.githubusercontent.com/P3TERX/aria2.conf/master/tracker.sh) "/root/.aria2/aria2.conf"
-sed -i "s/#export RCLONE_CONFIG=$HOME/.config/rclone/rclone.conf/export RCLONE_CONFIG=$HOME/.config/rclone/rclone.conf/g" /root/.aria2/rclone.env
+sed -i "s/#export RCLONE_CONFIG/export RCLONE_CONFIG/g" /root/.aria2/rclone.env
 aria2c --conf-path=/root/.aria2/aria2.conf
