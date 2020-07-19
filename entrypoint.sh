8603bd92e5d1092c4963f180a1da25ac93f48eb4
小朋 <2810580330@qq.com>
2020-07-19 15:29:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 date
 bash <(curl -fsSL https://raw.githubusercontent.com/P3TERX/aria2.conf/master/tracker.sh) "/root/.aria2/aria2.conf"
 sed -i "s/drive-name=OneDrive/drive-name=gd/g" /root/.aria2/script.conf
+
+sed -i "s/user-agent=Mozilla/#user-agent=Mozilla/g" /root/.aria2/aria2.conf
+sed -i "s/#user-agent=qBittorrent/user-agent=qBittorrent/g" /root/.aria2/aria2.conf
 aria2c --conf-path=/root/.aria2/aria2.conf
