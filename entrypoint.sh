ff907c499e99f0f8ea37642faeb3f275f5d5f2c8
小朋 <2810580330@qq.com>
2020-07-28 07:00:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ sed -i "s/drive-name=OneDrive/drive-name=gd/g" /root/.aria2/script.conf
 
 sed -i "s/user-agent=Mozilla/#user-agent=Mozilla/g" /root/.aria2/aria2.conf
 sed -i "s/#user-agent=qBittorrent/user-agent=qBittorrent/g" /root/.aria2/aria2.conf
-aria2c --conf-path=/root/.aria2/aria2.conf
+aria2c
