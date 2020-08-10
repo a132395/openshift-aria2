dead95fa18c4c1c5afc274fac5f78a529419b519
小朋 <2810580330@qq.com>
2020-08-10 16:17:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 #显示时间
 date
 bash <(curl -fsSL https://raw.githubusercontent.com/P3TERX/aria2.conf/master/tracker.sh) "/root/.aria2/aria2.conf"
-
+nohup caddy -conf="/etc/Caddyfile"   >/root/.aria2/caddy.txt 2>&1 &
 sed -i "s/drive-name=OneDrive/drive-name=gd/g" /root/.aria2/script.conf
 sed -i "s/user-agent=Mozilla/#user-agent=Mozilla/g" /root/.aria2/aria2.conf
 sed -i "s/#user-agent=qBittorrent/user-agent=qBittorrent/g" /root/.aria2/aria2.conf
